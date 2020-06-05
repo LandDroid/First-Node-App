9a83faec52471aaefdee49d69e73a40ab40daeea
LandDroid <landonlyle@live.ca>
2020-06-04 20:46:16 -0400

    Adding Index.js

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 const express = require('express');
 const app = express();
-import { get } from 'https';
 
 app.get('/', (request, response) => 
 {
-    get('https://insult.mattbas.org/api/insult', resp => 
-        {
-            resp.on('data', chunk => 
-                {
-                    response.send(`
-          ras              <http>
-                        <head><title>Oh Snap Landon</title></head>
-                        <body>
-                            <h1>OH SNAP!</h1>
-                            <p style="font-size: 24px; color: red;">
-                            ${chunk.toString()}!
-                            </p>
-                        </body>
-                        </http>
-                    `);
-                });
-        });
+    response.send("Hello, World");
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
