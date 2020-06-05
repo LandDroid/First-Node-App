8fb97189223d1d0ba8f8c99e288b03d61fd5aaed
LandDroid <landonlyle@live.ca>
2020-06-04 21:39:28 -0400

    Testing things

--- first-app-node.js
+++ first-app-node.js
@@ -2,46 +2,39 @@ const express = require('express');
 const app = express();
 const https = require('https');
 
+app.get('/', (request, response) => 
+    {
+        https.get('https://insult.mattbas.org/api/insult', resp => 
+            {
+                resp.on('data', chunk => 
+                    {
+                        response.send   (`
+                            <http>
+                                <head>
+                                    <title>Oh Snap Landon</title>
+                                </head>
+                                <body>
+                                    <h1>OH SNAP!</h1>
+                                        <p style="font-size: 24px; color: red;">${chunk.toString()}!</p>
 
-app.get('/', (request, response) => {
-https.get('https://insult.mattbas.org/api/insult', resp => {
-    resp.on('data', chunk => {
-        response.send(`
-        <http>
-                        <head><title>Oh Snap Landon</title></head>
-                        <body>
-                            <h1>OH SNAP!</h1>
-                            <p style="font-size: 24px; color: red;">
-                            ${chunk.toString()}!
-                            </p>
+                                    <h2>Button Click Counter</h2>
+                                        <p style="font-size: 14px;">
 
-                            <h2>JavaScript Alert</h2>
-
-<button onclick="myFunction()">Try it</button>
-
-<script>
-function myFunction() {
-  alert("I am an alert box!");
-}
-</script>
-                        </body>
-                        </http>
-        `);
-    })
-});
-});
+                                            <script type="text/javascript">
+                                            var clicks = 0;
+                                            function onClick() 
+                                                {
+                                                    clicks += 1;
+                                                    document.getElementById("clicks").innerHTML = clicks;
+                                                };
+                                             </script>
+                                    <button type="button" onClick="onClick()">Click me</button>
+                                        <p>Clicks: <a id="clicks">0</a></p>
+                                </body>
+                            </http>
+                                        `);
+                    })
+            });
+    });
   
-
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class Test extends React.Component {
-  render() {
-    return <h1>Hello World!</h1>;
-  }
-}
-
-ReactDOM.render(<Test />, document.getElementById('root'));
-
 app.listen(process.env.PORT || 3000);
